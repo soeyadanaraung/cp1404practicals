a4efe30e9c1e0408cceddcf2b2fb86194fd8f1f1
soeyadanaraung <soeyadanaraung@my.jcu.edu.au>
2024-09-29 18:46:15 +0800

    Determine score with function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,22 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = int(input("Enter score: "))
+    score_category = get_score_category(score)
+    print(score_category)
+    random_number = random.randint(1, 100)
+    print(random_number)
+
+def get_score_category(score):
+    if score < 0 or score > 100:
+        return "Invalid"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+random_number = random.randint(0,100)
+
+main()
+
