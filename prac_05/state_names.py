4e163cccd05aa8eda3667d7e63bbe66331234182
soeyadanaraung <soeyadanaraung@my.jcu.edu.au>
2024-10-20 20:15:17 +0800

    Complete prac 5 state names exercise.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,10 +9,12 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    # "Easier to Ask Forgiveness than Permission" (EAFP) Approach
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
