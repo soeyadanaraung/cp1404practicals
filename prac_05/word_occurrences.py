23ea94dbf504d0c56fe1cda064138c7f2cdce740
soeyadanaraung <soeyadanaraung@my.jcu.edu.au>
2024-11-03 17:16:39 +0800

    Write codes to determine the word occurrences.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,7 +8,10 @@ text = input("Text: ")
 words = text.split()
 words.sort()
 
+max_length = max(len(word) for word in words)
+
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 for word, count in word_to_count.items():
+    print(f"{word:{max_length}} : {count}")
